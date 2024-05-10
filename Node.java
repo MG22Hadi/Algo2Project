d922bd98f4b365b2f9dce796eb86b531b2d3a186
MG22Hadi <147649583+MG22Hadi@users.noreply.github.com>
2024-05-10 08:51:20 +0300

    nothing

--- Node.java
+++ Node.java
@@ -1,29 +1,35 @@
 public class Node {
     int height;
     int width;
-    Character name;
-    Tree left;
-    Tree right;
+    String name;
+    Node left;
+    Node right;
  
-    public Node(int height, int width, Character name, Tree left, Tree right) {
+    public Node(int height, int width, String name, Node left, Node right) {
        this.height = height;
        this.width = width;
        this.name=name;
        this.left = left;
        this.right = right;
     } 
-    public Node(int height, int width, Character name){
+    public Node(int height, int width, String name){
        this.height = height;
        this.width = width;
        this.name=name;
     }
 
+    public Node(String name) {
+        this.name = name;
+    }
+
     @Override
     public String toString() {
-        return "Node{" +
+        return " Node{" +
                 "height=" + height +
                 ", width=" + width +
                 ", name=" + name +
+                ", Right"+ right+
+                ", Left"+ left+
                 '}';
     }
 }
--- tree.java
+++ tree.java
@@ -3,38 +3,40 @@ import java.util.Stack;
 
 //(A[20,10] | (B[20,10]|C[30,10])) – (D[30,50] | (E[40,30] – F[40,20]))
 public class Tree {
-   ArrayList<Node> nodesList;
+   ArrayList nodesList;
    Stack<String> makeTreeStack;
+   Node root ;
+
    public void Import(String str){
       int x=0;
       String temp="";
       nodesList= new ArrayList();
 
       for(int i= 0;i<str.length();i++){
+         // تجريد العبارة من الارقام
          if(Character.isAlphabetic(str.charAt(i))||str.charAt(i)=='('||str.charAt(i)==')'||str.charAt(i)=='|'||str.charAt(i)=='-' ){
             temp+=str.charAt(i);
          }
-
+//;ssssssssss
          //Store nodes in ArrayList
          if (Character.isAlphabetic(str.charAt(i))){
 
-            String hightS=str.substring(i+2,i+4);
-            int hightI= Integer.parseInt(hightS);
+            String heightS =str.substring(i+2,i+4);
+            int heightI = Integer.parseInt(heightS);
             String widthS=str.substring(i+5,i+7);
             int widthI= Integer.parseInt(widthS);
-            Node newNode= new Node(hightI,widthI ,(str.charAt(i)));
+            Node newNode= new Node(heightI,widthI ,String.valueOf(str.charAt(i)));
             nodesList.add(newNode);
 
          }
       }
-      System.out.println(temp);
       makeTree(temp);
 
    }
    public void makeTree(String s){
       int sBracket=0, eBracket=0,indexF=0;
       String sub;
-      makeTreeStack=new Stack<>();
+      makeTreeStack=new Stack();
       for (int i= 0;i<s.length();i++){
          if(s.charAt(i)=='('&&sBracket==0) {
             sBracket++;
@@ -57,10 +59,15 @@ public class Tree {
             sBracket=eBracket=0;
          }
 
-
-
       }
       System.out.println(makeTreeStack.toString());
+      String t0=makeTreeStack.pop(),t1=makeTreeStack.pop(),t2=makeTreeStack.pop();
+      root =new Node(t2);
+      root.left=new Node(t1);
+      root.right=new Node(t0);
+
+      System.out.println(root.toString());
+
 
    }
   
